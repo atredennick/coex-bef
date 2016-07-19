b1ce5fc8d248c800c62434ef03fd90c224fa5e6c
Andrew Tredennick <atredenn@gmail.com>
2016-07-19 09:44:40 -0600

    updates pop model to repreoduce Gremer ELE results

--- code/wintann_pop_model.R
+++ code/wintann_pop_model.R
@@ -16,11 +16,13 @@ library(ggplot2)
 do_species <- "erla"
 
 ####
-####  ANNUAL PLANT POPULATION MODEL
+####  ANNUAL PLANT POPULATION MODEL -- DENSITY-INDEPENDENT
 ####
-project_popmod <- function(seed_surv, seeds_per_seedling, germination_frac)
+# From Gremer and Venable 2014 (Ecology Letters)
+project_popmod <- function(seed_surv_old, seed_surv_new, seeds_per_seedling, 
+                           germination_frac, nongerm_seed_surv)
 {
-  pgr <- seed_surv*(1-germination_frac) + seeds_per_seedling*germination_frac
+  pgr <- seeds_per_seedling*germination_frac*seed_surv_new + nongerm_seed_surv*seed_surv_old*(1-germination_frac)
 }
 
 
@@ -34,19 +36,38 @@ single_spp_data <- subset(species_year_data, species==do_species)
 
 
 ####
-####  MODEL LONG-TERM PER CAPITA GROWTH RATE
+####  SIMULATE LONG-TERM PER CAPITA GROWTH RATE
 ####
 single_spp_data <- subset(single_spp_data, germ.fraction!="." & lxbx!=".")
 seeds_per_seedling_vec <- as.numeric(as.character(single_spp_data$lxbx))
-seed_surv <- 0
+seed_surv_new <- 0.153
+seed_surv_old <- 0.56
+nongerm_seed_surv <- 0.823
 germination_frac_vec <- as.numeric(as.character(single_spp_data$germ.fraction))
-iters <- 100
+iters <- 1000
 pgr <- numeric(iters)
-for(t in 1:iters){
-  randyr <- sample(c(1:length(seeds_per_seedling_vec)), 1)
-  pgr[t] <- project_popmod(seed_surv = seed_surv, 
-                        seeds_per_seedling = seeds_per_seedling_vec[randyr],
-                        germination_frac = germination_frac_vec[randyr])
+germ_fracs <- seq(0,1,0.01)
+geom_mean <- numeric(length(germ_fracs))
+arith_mean <- numeric(length(germ_fracs))
+count <- 1
+gm_mean = function(x, na.rm=TRUE){
+  exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+}
+random_years <- sample(c(1:length(seeds_per_seedling_vec)), iters, replace = TRUE)
+for(germ in germ_fracs){
+  for(t in 1:iters){
+    randyr <- random_years[t]
+    pgr[t] <- project_popmod(seed_surv_old = seed_surv_old, 
+                             seed_surv_new = seed_surv_new, 
+                             seeds_per_seedling = seeds_per_seedling_vec[randyr],
+                             germination_frac = germ,
+                             nongerm_seed_surv = nongerm_seed_surv)
+  }
+  geom_mean[count] <- gm_mean(pgr)
+  arith_mean[count] <- log(mean(pgr[pgr>0]))
+  count <- count+1
 }
 
-plot(pgr, type="l")
+par(mfrow=c(1,2))
+plot(germ_fracs, geom_mean, type="l", main=do_species)
+plot(germ_fracs, arith_mean, type="l", main=do_species)
